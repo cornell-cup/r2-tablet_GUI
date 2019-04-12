bfb0c7db1638609fe96ee7b8ffdd191769384d6c
Haomiao Liu <haomiaoliu@dhcp-hol-2034.eduroam.cornell.edu>
2019-04-12 17:49:49 -0400

    adding another testing tab with textfield

--- app.py
+++ app.py
@@ -19,26 +19,27 @@ Builder.load_string("""
     do_default_tab: False
 
     TabbedPanelItem:
-        text: 'General Info' 
+        text: 'General Info'
         RstDocument:
             text:
                 '\\n'.join(("Hi there :)", "-----------",
                 "This is Cornell Cup. "
                 "I am R2. Nice to meet you!"))
-            
+
     TabbedPanelItem:
         text: 'Visual Img'
         BoxLayout:
             Label:
                 text: 'Second tab content area'
-                
+
+
     TabbedPanelItem:
         text: 'Streaming'
         RstDocument:
             text:
                 '\\n'.join(("Hello world", "-----------",
                 "You are in the third tab."))
-                
+
     TabbedPanelItem:
         text: 'Sign up'
         BoxLayout:
@@ -46,7 +47,30 @@ Builder.load_string("""
                 text: 'Second tab content area'
             Button:
                 text: 'Button that does nothing'
-    
+
+    TabbedPanelItem:
+        text: 'testing'
+        BoxLayout:
+
+            BoxLayout:
+                size_hint_y: 0.4
+                size_hint_x: .3
+                orientation: 'horizontal'
+
+                Label:
+                    text: "Enter your email"
+                    size_hint_x: .25
+                    spacing: .2, .2
+
+                TextInput:
+                    id: txt_searh
+                    text: ""
+                    size_hint_x: .3
+                    spacing: .2, .2
+
+                Label:
+                    id: lbl_search
+                    size_hint_y:None
 
 """)
 
