bc82046e4dc5ef0eb1be5d4c60ae045e88c1c4a4
zhanyanchen8 <zhanyanchen8@gmail.com>
2019-04-24 17:46:10 -0400

    trying to run main_program

--- main_program.py
+++ main_program.py
@@ -1,9 +1,23 @@
 import tkinterGUI
+import voiceRecognition
+import threading
 
 class main_program():
 
-    def __init__(self):
-        tkinterGUI.GUIapp()
-
+	def __init__(self):
+		voice_thread = threading.Thread(target = "voiceRecognition.main()", args = "")
+		gui_thread = threading.Thread(target = "tkinterGUI.GUIapp()", args = "")
+		
+		print ("stahting")
+		voice_thread.start()
+		print ("between statements")
+		gui_thread.start()
+		
+		print ("in between")
+		
+		voice_thread.join()
+		print ("ending statements")
+		gui_thread.join()
+		
 
 main_program()
