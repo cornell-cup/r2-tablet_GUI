7b8e0bf4b56d515f3322ce568210a087b105c07e
Yanchen Zhan '22 <zhanyanchen8@gmail.com>
2019-04-27 13:48:11 -0400

    device_index

--- voiceRecognition.py
+++ voiceRecognition.py
@@ -54,7 +54,7 @@ sleep_final = 997
 move_final = 996
 attendance_final = 995
 sentiment_value = 0
-
+device_index = 2
 
 def chunkify(arr):
     acc_total = []
@@ -341,7 +341,7 @@ def main():
 	
 	### opens microphone instance that takes speech from human to convert to text
 	r = sr.Recognizer()
-	mic = sr.Microphone(2)
+	mic = sr.Microphone(device_index)
 
 	# tells R2 to wake up
 	while (True):
