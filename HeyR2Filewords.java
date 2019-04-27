1c7538f08a118c14d3c769baa33b64324c23097a
Varun <vm324@cornell.edu>
2019-04-27 12:33:54 -0400

    hey r2

--- HeyR2Filewords.java
+++ HeyR2Filewords.java
@@ -3,12 +3,14 @@ import java.util.Set;
 
 public class HeyR2Filewords {
   public static void main(String[] args) {
-    String words = "ai2,hey r2,zr2,,airtel,naruto,airtel,airtel,airtel,ar jail,hey r2,hey r2,ar2";
+    String words = "ai2,hey r2,zr2,,airtel,naruto,airtel,airtel,airtel,ar jail,hey r2,hey r2,ar2, arto";
     String[] arr = words.split("\\s*,\\s*");
     Set<String> set = new HashSet();
     for (String s : arr)
       set.add(s);
+    System.out.print("[");
     for (String s : set)
-      System.out.println(s);
+      System.out.print(s + ",");
+    System.out.print("]");
   }
 }
