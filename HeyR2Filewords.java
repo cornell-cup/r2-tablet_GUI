c32a20079a181f585b7bf9104feb9d0fd38dd5c8
Varun <vm324@cornell.edu>
2019-04-27 12:35:54 -0400

    weird mic

--- HeyR2Filewords.java
+++ HeyR2Filewords.java
@@ -3,7 +3,7 @@ import java.util.Set;
 
 public class HeyR2Filewords {
   public static void main(String[] args) {
-    String words = "ai2,hey r2,zr2,,airtel,naruto,airtel,airtel,airtel,ar jail,hey r2,hey r2,ar2, arto";
+    String words = "ai2,hey r2,zr2,,airtel,naruto,airtel,airtel,airtel,ar jail,hey r2,hey r2,ar2,they are two,zr2,ar2,hey r2,ar2,ar-10,hey r2,hey arthur,ar2,q38,q38,ko2,hey r2,you are two,hey r2,hey r2,halo 2,rdr2 hello,hey or two,or to blue,yo are two";
     String[] arr = words.split("\\s*,\\s*");
     Set<String> set = new HashSet();
     for (String s : arr)
