cca8df763c4809054d634e9b62b1f086a6cb8271
shaneelu <sl2363@cornell.edu>
2019-04-28 19:18:38 -0400

    Update strongarm.py

--- strongarm.py
+++ strongarm.py
@@ -5,6 +5,6 @@ ser_strong = 0
 
 def run():
     ser_strong = serial.Serial('/dev/strongarm', 9600)
-    ser_strong.write("M2DP595E".encode('utf-8'))
+    ser_strong.write("M2DN595E".encode('utf-8'))
     
 run()
