3f9af6570a91ed85c99897ee2858a6f51f45de7b
shaneelu <sl2363@cornell.edu>
2019-04-28 18:10:01 -0400

    Update voiceRecognitionTextTest.py

--- voiceRecognitionTextTest.py
+++ voiceRecognitionTextTest.py
@@ -20,9 +20,9 @@ import piimages_final
 import socket
 import json
 import time
-import nerf
+#import nerf
 import locomotion
-import precisearm
+#import precisearm
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
     import Features, EntitiesOptions, KeywordsOptions, SentimentOptions
