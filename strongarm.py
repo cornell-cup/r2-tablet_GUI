7724f2cb7240921dbc48c16345c98f458fc3dc1e
Yanchen Zhan <zhanyanchen8@gmail.com>
2019-04-29 04:12:23 +0000

    change files

--- strongarm.py
+++ strongarm.py
@@ -8,8 +8,8 @@ def run():
     time.sleep(1)
     while(True):
         time.sleep(1)
-        ser_strong.write("M2DP595E".encode('utf-8'))
-        time.sleep(1)
         ser_strong.write("M2DP300E".encode('utf-8'))
+        #time.sleep(1)
+        #ser_strong.write("M2DP300E".encode('utf-8'))
     ser_strong.close()
 run()
