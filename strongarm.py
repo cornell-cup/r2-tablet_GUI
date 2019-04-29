5fc649615c9fe62d63cf812272b69b52ab499b06
Yanchen Zhan <zhanyanchen8@gmail.com>
2019-04-29 06:19:25 +0000

    asdf

--- strongarm.py
+++ strongarm.py
@@ -1,3 +1,4 @@
+
 import serial
 import time
 
@@ -6,10 +7,17 @@ ser_strong = serial.Serial('/dev/strongarm', 9600)
 
 time.sleep(1)
 while(True):
-    ser_strong.write("M1DN000E".encode('utf-8'))
-    time.sleep(1)
-    ser_strong.write("M2DP100E".encode('utf-8'))
-    time.sleep(1)
+    ser_strong.write("M2DN999E".encode('utf-8'))
+    time.sleep(2)
+    ser_strong.write("M2DN800E".encode('utf-8'))
+    time.sleep(2)
+    #ser_strong.write("M2DN595E".encode('utf-8'))
+    #time.sleep(1)
+    #ser_strong.write("M1DN625E".encode('utf-8'))
+    #time.sleep(1)
+    #ser_strong.write("M2DP595E".encode('utf-8'))
+    #time.sleep(1)
+    
     #if ser_strong.inWaiting():
     #print(ser_strong.readline())
 # ser_strong.close()
